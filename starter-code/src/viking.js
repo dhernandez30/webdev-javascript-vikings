44ed4d9cafdfbdf5d684fd0a5680b75e142ea30a
Daniel Hernandez <dhernandez20167@columbushs.com>
2019-12-10 21:31:42 -0500

    almost

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,36 +1,75 @@
 // Soldier
 class Soldier {
     constructor(health,strength){
-    this.health = health;
-    this.strength = strength;
-    }
-  
-    attack() {
-    return this.strength;
+    this.health=health;
+    this.strength=strength
     }
 
-    receiveDamage(damage){
-        this.health = this.health - damage ;
-    
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(thedamage) {
+        this.health -= thedamage;
+
     }
 }
 
-// Viking  
+// Viking
 class Viking extends Soldier {
     constructor(name,health,strength){
         super(health,strength);
-        this.name = name;
-        this.health = health;
-        this.strength = strength;
+        this.name=name
     }
+    receiveDamage(thedamage){
+        this.health -= thedamage;
+        if(this.health > 0){
+            return `${this.name} has received ${thedamage} points of damage`
+        }
+        else if(this.health === 0){
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!"
 }
     
-
-      
-    
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+   constructor(health,strength){
+        super(health,strength);
+   }
+   receiveDamage(thedamage){
+       this.health -= thedamage;
+       
+       if(this.health > 0){
+        return `A Saxon has received ${thedamage} points of damage`
+    }
+    else if(this.health === 0){
+        return `A Saxon has died in combat`
+   }
+}
+    
+}
 
 // War
-class War {}
+class War {
+
+    vikingArmy = [];
+    saxonArmy = [];
+
+    addViking(vikingo){
+        this.vikingArmy.push(vikingo);
+    }
+
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon);
+    }
+
+    vikingAttack(){
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        
+    }}
